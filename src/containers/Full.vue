ed32ae9a6f05748eeea54eb260215ec259eccc96
sengatang <sengattt@gmail.com>
2018-04-12 15:23:31 +0800

    新增登陆

--- src/containers/Full.vue
+++ src/containers/Full.vue
@@ -33,6 +33,134 @@ export default {
       nav: nav.items
     }
   },
+  mounted: function () {
+    var that = this
+    console.log(that.nav)
+    if (this.$route.params.user_type === 0) {
+      that.nav.items = [
+        {
+          name: 'Dashboard',
+          url: '/dashboard',
+          icon: 'icon-speedometer',
+          badge: {
+            variant: 'primary',
+            text: 'NEW'
+          }
+        }
+      ]
+      that.nav.push(
+        {
+          title: true,
+          name: '后台系统',
+          wrapper: {
+            element: 'span',
+            attributes: {}
+          },
+          class: ''
+        },
+        {
+          name: '病例管理',
+          url: '/casesManagement',
+          icon: 'icon-calculator'
+        },
+        {
+          name: '病种管理',
+          url: '/diseaseManagement',
+          icon: 'icon-calculator'
+        },
+        {
+          name: '试题管理',
+          url: '/quizManagement',
+          icon: 'icon-calculator'
+        },
+        {
+          name: '用户管理',
+          url: '/userManagement',
+          icon: 'icon-calculator'
+        },
+        {
+          name: '系统功能管理',
+          url: '/system',
+          icon: 'icon-star',
+          children: [
+            {
+              name: '科室管理',
+              url: '/system/department',
+              icon: 'icon-star'
+            },
+            {
+              name: '疫苗管理',
+              url: '/system/vaccine',
+              icon: 'icon-star'
+            },
+            {
+              name: '药品管理',
+              url: '/system/medicine',
+              icon: 'icon-star'
+            }
+          ]
+        }
+      )
+    }
+    if (this.$route.params.user_type > 0) {
+      that.nav.items = [
+        {
+          name: 'Dashboard',
+          url: '/dashboard',
+          icon: 'icon-speedometer',
+          badge: {
+            variant: 'primary',
+            text: 'NEW'
+          }
+        }
+      ]
+      that.nav.push(
+        {
+          title: true,
+          name: '前台系统',
+          wrapper: {
+            element: 'span',
+            attributes: {}
+          },
+          class: ''
+        },
+        {
+          name: '职能学习',
+          url: '/study',
+          icon: 'icon-puzzle',
+          children: [
+            {
+              name: '角色扮演',
+              url: '/study/cosplay',
+              icon: 'icon-puzzle'
+            },
+            {
+              name: '病例学习',
+              url: '/study/cases',
+              icon: 'icon-puzzle'
+            }
+          ]
+        },
+        {
+          name: '3D导览',
+          url: '/threeD',
+          icon: 'icon-calculator',
+          badge: {
+            variant: 'danger',
+            text: 'NEW'
+          }
+        },
+        {
+          name: '考试',
+          url: '/quiz',
+          icon: 'icon-calculator'
+        },
+        {
+          divider: true
+        }
+      )
+    }
+  },
   computed: {
     name () {
       return this.$route.name
@@ -41,5 +169,10 @@ export default {
       return this.$route.matched
     }
   }
+  // ,
+  // methods: {
+  //   clean(nav) {
+  //   }
+  // }
 }
 </script>
--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -1,21 +1,21 @@
 <template>
   <el-container>
     <el-main>
-      <el-card class="box-card">
+      <el-card class="login-card">
         <el-row>
           <el-col :span="12">
-            <el-form ref="form" :model="form" label-width="80px">
+            <el-form class="login-form" :model="loginForm" :rules="loginRules" ref="loginForm" label-width="80px">
               <el-form-item>
-                <h1>Login</h1>
+                <h3>Welcome PetHospital</h3>
               </el-form-item>
-              <el-form-item label="用户名">
-                <el-input v-model="form.name"></el-input>
+              <el-form-item label="用户名" prop="username">
+                <el-input v-model="loginForm.username" autoComplete="on" placeholder="username"></el-input>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input v-model="form.password"></el-input>
+              <el-form-item label="密码" prop="password">
+                <el-input :type="passwordType" v-model="loginForm.password" autoComplete="on" placeholder="password"></el-input>
               </el-form-item>
               <el-form-item>
-                <el-button  type="primary" @click="onSubmit">登陆</el-button>
+                <el-button type="primary" :loading="loading" @click.native.prevent="handleLogin">登陆</el-button>
                 <el-button>忘记密码</el-button>
                 </el-form-item>
             </el-form>
@@ -30,16 +30,44 @@
 export default {
   name: 'Login',
   data () {
-    return {
-      form: {
-        name: '',
-        password: ''
+    const validatePassword = (rule, value, callback) => {
+      if (value.length < 6) {
+        callback(new Error('The password can not be less than 6 digits'))
+      } else {
+        callback()
       }
+    }
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginRules: {
+        username: [{ required: true, trigger: 'blur' }],
+        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+      },
+      passwordType: 'password',
+      loading: false,
+      showDialog: false
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    handleLogin () {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.loading = true
+          this.$http.post('http://112.74.48.64:80/user/login', {name: this.loginForm.username, password: this.loginForm.password}).then(response => {
+            if (response.body.status === 'success') {
+              this.loading = false
+              console.log(response.body.data.role)
+              this.$router.push({ name: 'Dashboard', params: {user_type: response.body.data.role} })
+            }
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
     }
   }
 }
