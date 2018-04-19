2111e2ea89be09a0e74a0101f7d2b8fe00c67731
senga <sengattt@gmail.com>
2018-04-19 15:59:08 +0800

    3d示例

--- src/views/ThreeD.vue
+++ src/views/ThreeD.vue
@@ -5,7 +5,13 @@
       <span>3D</span>
       </div>
      <p>Welcome To 3D</p>
-     <iframe width="600" id="" height="400" allowfullscreen style="border-style:none;" src="http://cdn.pannellum.org/2.4/pannellum.htm#panorama=http%3A//i1.bvimg.com/642187/7bafd4e5322607ea.jpg"></iframe>
+     <el-button @click="dialogVisible = true">test</el-button>
+     <el-dialog
+        title="提示"
+        :visible.sync="dialogVisible"
+        :before-close="handleClose">
+        <iframe width="600" height="400" allowfullscreen style="border-style:none;" src="http://cdn.pannellum.org/2.4/pannellum.htm#panorama=http%3A//i1.bvimg.com/642187/7bafd4e5322607ea.jpg"></iframe>
+      </el-dialog>
     </el-card>
   </div>
 </template>
@@ -15,6 +21,16 @@ export default {
   name: 'threeD',
   data () {
     return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleClose (done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
     }
   }
 }
